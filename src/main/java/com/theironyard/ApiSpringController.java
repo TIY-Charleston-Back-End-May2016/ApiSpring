b66dc7eb473264c33e2202874ad20b2082efb176
oakes <zsoakes@gmail.com>
2016-07-25 10:50:43 -0400

    Add background thread

--- src/main/java/com/theironyard/ApiSpringController.java
+++ src/main/java/com/theironyard/ApiSpringController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 
 /**
@@ -14,8 +15,28 @@ import java.util.HashMap;
 public class ApiSpringController {
     static final String API_URL = "http://gturnquist-quoters.cfapps.io/api/random";
 
+    @PostConstruct
+    public void init() throws InterruptedException {
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println("Hello, world!");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.start();
+    }
+
     @RequestMapping(path = "/quote", method = RequestMethod.GET)
     public HashMap getQuote() {
+        // see more complex example here:
+        // https://github.com/Book-It/BookIt/blob/master/src/main/java/com/theironyard/controllers/BookItController.java#L309
         RestTemplate query = new RestTemplate();
         HashMap result = query.getForObject(API_URL, HashMap.class);
         String type = (String) result.get("type");
