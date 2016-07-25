1027cdcb447ae6c2a506e71b3782a3df73266e1b
oakes <zsoakes@gmail.com>
2016-07-25 11:02:58 -0400

    Add async method

--- src/main/java/com/theironyard/ApiSpringController.java
+++ src/main/java/com/theironyard/ApiSpringController.java
@@ -1,12 +1,18 @@
 package com.theironyard;
 
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * Created by zach on 7/25/16.
@@ -33,8 +39,8 @@ public class ApiSpringController {
         t.start();
     }
 
-    @RequestMapping(path = "/quote", method = RequestMethod.GET)
-    public HashMap getQuote() {
+    @Async
+    public Future<HashMap> requestQuote() {
         // see more complex example here:
         // https://github.com/Book-It/BookIt/blob/master/src/main/java/com/theironyard/controllers/BookItController.java#L309
         RestTemplate query = new RestTemplate();
@@ -42,8 +48,25 @@ public class ApiSpringController {
         String type = (String) result.get("type");
         if (type.equals("success")) {
             HashMap value = (HashMap) result.get("value");
-            return value;
+            return new AsyncResult<>(value);
         }
-        return null;
+        return new AsyncResult<>(null);
+    }
+
+    @RequestMapping(path = "/quote", method = RequestMethod.GET)
+    public ArrayList getQuote() throws InterruptedException, ExecutionException {
+        Future<HashMap> quote1 = requestQuote();
+        Future<HashMap> quote2 = requestQuote();
+        Future<HashMap> quote3 = requestQuote();
+
+        while (!quote1.isDone() || !quote2.isDone() || !quote3.isDone()) {
+            Thread.sleep(100);
+        }
+
+        ArrayList arr = new ArrayList();
+        arr.add(quote1.get());
+        arr.add(quote2.get());
+        arr.add(quote3.get());
+        return arr;
     }
 }
